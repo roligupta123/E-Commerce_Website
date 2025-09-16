de4f2fc91a2711502feae1fbcc20dae9b2f1d7c4
Anchal <mauryaanchal756@gmail.com>
2025-09-16 10:22:23 +0530

    Updated database

--- Database/Routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cartController = require('../Controllers/cartController');
-
-router.post('/add', cartController.addToCart);
-router.get('/:user', cartController.getCart);
-router.post('/remove', cartController.removeItem);
-
-module.exports = router;
