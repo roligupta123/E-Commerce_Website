0e00240bad98b5e9fa86f62cc53199b8d0f1e023
zanyKhan <fatmazainab071@gmail.com>
2025-09-24 10:13:35 +0530

    Specific product

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -7,5 +7,23 @@ async function getProducts(req, res) {
     res.json(data)
 }
 
-module.exports = {getProducts}
+//Get product by Id
+
+async function getProductById(req, res) {
+    try {
+        const productId = req.params.id;
+        const product = await Product.findById(productId);
+
+        if (!product) {
+        return res.status(404).json({ error: "Product not found" });
+        }
+        res.json(product)
+
+    } catch (error) {
+        console.log("Found an error : ", error.message);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = { getProducts, getProductById }
 
--- backend/routes/products/products.js
+++ backend/routes/products/products.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router();
 const products = require("../../data/products.json")
-const {getProducts} = require("../../controller/productController");
+const {getProducts, getProductById} = require("../../controller/productController");
 
 
 router.get("/products", getProducts);
+router.get("/product/:id", getProductById);
 
 module.exports = router;
