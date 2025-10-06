85dc597ad249e4bdd32d2bfa7e079d78ed3823b7
zanyKhan <fatmazainab071@gmail.com>
2025-10-06 07:35:38 +0530

    Changes in cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,12 @@ const app = express();
 
 connection();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true, 
+    }
+));
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
